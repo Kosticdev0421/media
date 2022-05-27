22d147f1b2ecb6eba659c5181b43d8c228703d23
Markodev0421 <mpribilovic2@gmail.com>
2022-05-27 19:46:26 +0530

    feat: added mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import PersonIcon from '@mui/icons-material/Person';
 
 function App() {
   return (
     <div className="App">
-      hi
+      <PersonIcon/>
     </div>
   );
 }
