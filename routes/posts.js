e31b27eb53e5f4a37fd5fe592bc0fde2916dca02
Markodev0421 <mpribilovic2@gmail.com>
2022-05-27 17:49:42 +0530

    build: like and dislike post

--- routes/posts.js
+++ routes/posts.js
@@ -39,8 +39,21 @@ router.delete("/:id", async (req, res) => {
       res.status(500).json(err);
     }
   });
-//like
-
+//like dislike
+router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post.likes.includes(req.body.userId)) {
+        await post.updateOne({ $push: { likes: req.body.userId } });
+        res.status(200).json("The post has been liked");
+      } else {
+        await post.updateOne({ $pull: { likes: req.body.userId } });
+        res.status(200).json("The post has been disliked");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 //get a post
 
 //get all post of followings
