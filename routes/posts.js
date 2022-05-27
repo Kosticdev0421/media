3b0eafc47878c31606dff95b1ecc4e372767fb38
Markodev0421 <mpribilovic2@gmail.com>
2022-05-27 18:34:53 +0530

    build: updated get all posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post")
+const User = require("../models/User"); 
 
 //create a post
 router.post("/",async(req,res)=>{
@@ -64,4 +65,18 @@ router.put("/:id/like", async (req, res) => {
     }
   });
 //get all post of followings
+router.get("/timeline/all", async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.body.userId);
+      const userPosts = await Post.find({ userId: currentUser._id });
+      const friendPosts = await Promise.all(
+        currentUser.followings.map((friendId) => {
+          return Post.find({ userId: friendId });
+        })
+      );
+      res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 module.exports = router;
