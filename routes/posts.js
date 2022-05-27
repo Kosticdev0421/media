10e491307c6d45c2491594688298b8d2c932d6e1
Markodev0421 <mpribilovic2@gmail.com>
2022-05-27 17:34:25 +0530

    build: updated post

--- routes/posts.js
+++ routes/posts.js
@@ -12,9 +12,33 @@ router.post("/",async(req,res)=>{
   }
 })
 //update 
-
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("the post has been updated");
+      } else {
+        res.status(403).json("you can update only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 //delete
-
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.deleteOne();
+        res.status(200).json("the post has been deleted");
+      } else {
+        res.status(403).json("you can delete only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 //like
 
 //get a post
