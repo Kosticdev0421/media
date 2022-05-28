8474bd9cac8729189b8ff3de1f745571f862fbe9
Markodev0421 <mpribilovic2@gmail.com>
2022-05-28 18:16:01 +0530

    build: updated feed

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Share from '../share/Share'
 import "./Feed.css"
 
 const Feed = () => {
   return (
-    <div className="feed">Feed</div>
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share/>
+      </div>
+    </div>
   )
 }
 
