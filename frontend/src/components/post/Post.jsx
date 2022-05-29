71c4c0d07bf3a3b42e0b09a8f8a36f9b71ece394
Markodev0421 <mpribilovic2@gmail.com>
2022-05-29 19:41:33 +0530

    build: updated post

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,9 +1,15 @@
 import "./Post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Users } from "../../dummyData.js";
+import { useState } from "react";
 
 export default function Post({post}) {
-   
+   const [like,setLike] = useState(post.like)
+   const [isLiked,setIsLiked] = useState(false)
+   const likeHandler=() =>{
+     setLike(isLiked? like-1: like+1)
+     setIsLiked(!isLiked)
+   }
   return (
     <div className="post">
       <div className="postWrapper">
@@ -29,9 +35,9 @@ export default function Post({post}) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
-            <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">{post.like} people like it</span>
+            <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
+            <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment}comments</span>
