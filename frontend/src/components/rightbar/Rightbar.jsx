5280b29f87289d9bb7ae21b25d0ebe3119bfdab8
Markodev0421 <mpribilovic2@gmail.com>
2022-05-29 09:05:14 +0530

    build: updated right bar

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -1,10 +1,31 @@
+
+
 import React from 'react'
 import "./Rightbar.css"
 
-const Rightbar = () => {
+export default function Rightbar(){
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+       <div className="rightbarWrapper">
+         <div className="birthdayContainer">
+           <img src="/assets/gift.png" alt="" className="birthdayImg" />
+           <span className="birthdayText">
+             <b>Bunny</b> and <b>3 others have a</b> birthday today
+           </span>
+         </div>
+         <img src="/assets/ad.png" alt="" className="rightbarAd" />
+         <h4 className="rightbarTitle">Online Friends</h4>
+         <ul className="rightbarFriendList">
+           <li className="rightbarFriend">
+                <div className="rightbarProfileImgContainer">
+                  <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">Bhavya Vadavalli</span>
+           </li>
+         </ul>
+       </div>
+    </div>
   )
 }
 
-export default Rightbar
