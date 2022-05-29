c7aa10a5508e571b9595428274c0414efe211fb0
Markodev0421 <mpribilovic2@gmail.com>
2022-05-30 00:31:15 +0530

    build: updated profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Profile/>
     </div>
   );
 }
---
+++ frontend/src/pages/profile/Profile.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Topbar from '../../components/topbar/Topbar'
+import "./Profile.css"
+
+function Profile() {
+  return (
+    <>
+    <Topbar />
+    <div className="profile">
+      <Sidebar />
+      <div className="profileRight">
+        <div className="profileRightTop">
+          <div className="profileCover">
+            <img
+              className="profileCoverImg"
+              src="assets/post/3.jpeg"
+              alt=""
+            />
+            <img
+              className="profileUserImg"
+              src="assets/person/7.jpeg"
+              alt=""
+            />
+          </div>
+          <div className="profileInfo">
+              <h4 className="profileInfoName">Lalitha Vadavalli </h4>
+              <span className="profileInfoDesc">Hello my friends!</span>
+          </div>
+        </div>
+        <div className="profileRightBottom">
+          <Feed />
+          <Rightbar profile/>
+        </div>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Profile
