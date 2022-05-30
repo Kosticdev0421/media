2608cf56fbdfae222fb7575db7ba0203e8af41ab
Markodev0421 <mpribilovic2@gmail.com>
2022-05-30 08:46:14 +0530

    build: added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
 
 import './App.css';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Profile from './pages/profile/Profile';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+   <Router>
+     <Switch>
+       <Route exact path="/">
+        <Home/>
+       </Route>
+       <Route path="/login">
+        <Login />
+       </Route>
+       <Route path="/register">
+        <Register/>
+       </Route>
+       <Route path="/profile/:username">
+        <Profile />
+       </Route>
+     </Switch>
+   </Router>
   );
 }
 
